a7cbe3afb3fe831c7bf50074114225e8a3669dcb
MrDalo <dadokralik@gmail.com>
2022-02-23 15:30:41 +0100

    Add syncWait function

--- follow.js
+++ follow.js
@@ -1,13 +1,25 @@
 let mainInterval;
 let array;
 
+function synWait(ms){
+    const end = Date.now() + ms
+    while (Date.now() < end)
+    {
+        //clear WHILE setting sleep
+    }
+}
+
 function follows()
 {
-   
+    //array = [1,2,3,4,5,6];
     array = document.querySelectorAll("button.sqdOP.L3NKy.y3zKF");
-    console.log(array.length);
+    console.log("Dlzka array: ", array.length);
+    
 
-    array.forEach(item =>{item.click()});
+    array.forEach(item =>{
+        item.click();
+        console.log("Clicked");
+        synWait(1000);});
     document.querySelector(".isgrP").scrollBy(0, 300); 
 }
 
